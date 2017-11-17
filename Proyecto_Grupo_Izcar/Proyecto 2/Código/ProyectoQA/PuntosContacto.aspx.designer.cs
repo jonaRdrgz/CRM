66e50a596996e5c2748009b01251246b180d092a
MeliMolina <melimolina04@hotmail.com>
2017-11-17 15:28:11 -0600

    Algunos cambios y Puntos de Contacto listo

--- Proyecto_Grupo_Izcar/Proyecto 2/Código/ProyectoQA/PuntosContacto.aspx.designer.cs
+++ Proyecto_Grupo_Izcar/Proyecto 2/Código/ProyectoQA/PuntosContacto.aspx.designer.cs
@@ -10,15 +10,15 @@
 namespace ProyectoQA {
     
     
-    public partial class ProductosRelacionados {
+    public partial class PuntosContacto {
         
         /// <summary>
-        /// vistaProductosRelacionados control.
+        /// verVendedores control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl vistaProductosRelacionados;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl verVendedores;
     }
 }
