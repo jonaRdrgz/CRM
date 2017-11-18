2c0c40520e916b0ba79e7d5651b2df14aaab0571
MeliMolina <melimolina04@hotmail.com>
2017-11-17 19:08:23 -0600

    Cambios Modelo y Edicion de estados de reportes de errores

---
+++ Proyecto_Grupo_Izcar/Proyecto 2/Código/ProyectoQA/RegistrarEstadoReportes.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/home.Master" AutoEventWireup="true" CodeBehind="RegistrarEstadoReportes.aspx.cs" Inherits="ProyectoQA.RegistrarEstadoReportes" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder4" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="col-lg-12" >
+        <h3>Registrar Estado del Producto</h3>
+                        
+        <form method="post" class="form-horizontal" runat ="server" display="Dynamic">
+
+            <div class="form-group">
+                <label class="control-label" >Fecha del reporte</label>
+                <asp:TextBox ID="fecha" CssClass ="form-control" runat="server" type="date" required=""></asp:TextBox>    
+            </div>
+        
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <label class="control-label">Estado</label>
+                <asp:DropDownList ID="ddEstado" CssClass ="form-control" runat="server" required="">
+                    <asp:ListItem value ="Noespecificado">No especificado</asp:ListItem>
+                    <asp:ListItem value ="Reparado">Reparado</asp:ListItem>
+                    <asp:ListItem value ="NoReparado">No Reparado</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <div class="hr-line-dashed"></div>                        
+            <div class="form-group">
+                <label class="control-label" >Diagnóstico</label>
+                <asp:TextBox ID="diagnostico" CssClass ="form-control" runat="server" required="" value=""></asp:TextBox>
+            </div>
+
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <div class="col-sm-8">
+                    <asp:Button ID="registrarEstadoButton" type="submit" Onclick ="registrarEstadoReporte" href ="ReportesErroresVendedor.aspx" CssClass ="btn btn-primary" runat="server" Text ="Registrar estado del producto" />
+                    <asp:LinkButton ID="cancelarRespuesta" CssClass="btn btn-default" href="javascript:history.go(-1)"  runat="server" Text="Cancelar" />
+                </div>
+            </div>
+
+        </form>
+
+    </div>
+</asp:Content>
---
+++ Proyecto_Grupo_Izcar/Proyecto 2/Código/ProyectoQA/ReportesErroresVendedor.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Data;
+using ProyectoQA.Classes;
+using System.Windows.Forms;
+
+namespace ProyectoQA
+{
+    public partial class ReportesErroresVendedor : System.Web.UI.Page
+    {
+       
+            private IConexion conexion;
+
+            public ReportesErroresVendedor()
+            {
+                //conexion = new Conexion("icampos.me", "mydb", "root", "nT4LZIYR5LYzoHAjAKtw", "32769");
+                conexion = new Conexion("localhost", "mydb", "root", "root", "3306");
+            }
+
+            public ReportesErroresVendedor(IConexion pConexion)
+            {
+                conexion = pConexion;
+            }
+
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                //conexion = new Conexion("localhost", "mydb", "root", "", "3306");
+                //conexion = new Conexion("icampos.me", "mydb", "root", "nT4LZIYR5LYzoHAjAKtw", "32769");
+                //popularProductos();
+            }
+
+       
+
+        //Vista (Probado)
+        public Boolean consultarReportesVendedor(String pIdUsuario, HtmlGenericControl etiqueta)
+            {
+                String reportes = "";
+                try
+                {
+                    conexion.AbrirConexion();
+                    conexion.setCommandText("call getReportesErroresVendedores('" + pIdUsuario + "');");
+                    IDataReader resultadosQuery = conexion.getResultados();
+                    reportes = crearVistaReporteErrorVendedores(resultadosQuery);
+                    conexion.CerrarConexion();
+                    GUIBuilder.inyectarHTML(reportes, etiqueta);
+                    return true;
+                }
+                catch (MySqlException ex)
+
+                {
+                    MessageBox.Show("Falló la operación " + ex.Message);
+                    return false;
+                }
+            }
+            public string crearVistaReporteErrorVendedores(IDataReader reader)
+            {
+                String nombreProducto;
+                String descripcion;
+                String fecha;
+                String cliente;
+               
+                String estado;
+                String diagnostico;
+                String idReporte;
+
+            String reporte = "";
+                while (reader.Read())
+                {
+                    nombreProducto = reader[0].ToString();
+                    descripcion = reader[1].ToString();
+                    fecha = reader[2].ToString();
+                    cliente = reader[3].ToString();
+                    estado = reader[4].ToString();
+                    diagnostico = reader[5].ToString();
+                    idReporte = reader[6].ToString();
+
+
+                reporte += "<tr>" +
+                                    "<td>" + nombreProducto + "</td>" +
+                                    "<td>" + descripcion + "</td>" +
+                                    "<td>" + fecha + "</td>" +
+                                    "<td>" + cliente + "</td>" +
+                                    "<td>" + estado + "</td>" +
+                                    "<td>" + diagnostico + "</td>" +
+                                    "<td>" + "<a href='RegistrarEstadoReportes.aspx?idReporte=" + idReporte + "'>Editar estado del reporte</a>" + "</td>" +
+                                "</tr>";
+                }
+                return reporte;
+
+
+            }
+            public void verReporteErroresVendedor(object sender, EventArgs e)
+            {
+                String idUsuario = Session["idUsuario"].ToString();
+
+                if (consultarReportesVendedor(idUsuario, vistaReporteErroresVendedor))
+                {
+                    Page_Load(sender, e);
+                }
+                else
+                {
+
+                    Verificador.mostrarMensaje(Page);
+                }
+            }
+
+        
+        }
+
+}
