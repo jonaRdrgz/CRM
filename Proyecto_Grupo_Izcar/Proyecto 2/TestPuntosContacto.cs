145770f3f76c04fdba6c69743846b0c2ff49d21a
MeliMolina <melimolina04@hotmail.com>
2017-11-18 20:57:42 -0600

    Pruebas de Puntos de contacto y Reporte de Errores

---
+++ Proyecto_Grupo_Izcar/Proyecto 2/TestPuntosContacto.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoQA.Tests
+{
+    [TestFixture]
+    public class TestPuntosContacto
+    {
+        [TestCase]
+        public void consultarVendedores_visualizarContactos_ReturnListNull()
+        {
+            FakePuntosdeContacto instancia = new FakePuntosdeContacto();
+            List<Usuario> resultado = instancia.consultarVendedores();
+            Assert.AreEqual(resultado, null);
+        }
+
+    }
+}
